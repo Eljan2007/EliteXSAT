3b38d6d23f7aa35d71454ecfadc69ab323767ae9
EliteXSAT <elcanehmedli2007@gmail.com>
2026-07-05 13:25:39 +0800

    Update 07/05/2026 13:25:39.84

---
+++ Publish-Update.cmd
@@ -0,0 +1,34 @@
+@echo off
+title EliteXSAT - Publish update
+cd /d "%~dp0"
+
+where git >nul 2>nul || (
+  echo.
+  echo   Git was not found. Install it once from https://git-scm.com  then run this again.
+  echo.
+  pause
+  exit /b
+)
+
+echo.
+echo   ==================================================
+echo     Publishing your latest EliteXSAT changes online
+echo   ==================================================
+echo.
+
+git add -A
+git commit -m "Update %date% %time%"
+git push origin main
+
+echo.
+echo   ------------------------------------------------------------
+echo   If you saw no red errors above, you're done. Your live site
+echo   refreshes in about a minute at:
+echo.
+echo     https://eljan2007.github.io/EliteXSAT/app.html
+echo.
+echo   (If it said "nothing to commit" - that just means there were
+echo    no changes since last time. Totally fine.)
+echo   ------------------------------------------------------------
+echo.
+pause
